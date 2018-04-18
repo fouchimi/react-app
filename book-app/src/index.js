a7aa168e68c89a926690e94b562903a43d36db45
Omar Mefire <omefire@gmail.com>
2018-04-18 00:49:13 -0500

    1- Fix bug in 'Add Book' feature 2- Redirect to a different component after a book is added.

--- book-app/src/index.js
+++ book-app/src/index.js
@@ -2,8 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import App from './App';
+import App, { Bookshelves } from './App';
 import registerServiceWorker from './registerServiceWorker';
+import {
+    BrowserRouter as Router,
+    Route
+} from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+    <Router>
+        <div>
+            <App />
+            <Route path="/bookshelves" component={Bookshelves} />
+        </div>
+    </Router>,
+    document.getElementById('root'));
 registerServiceWorker();
