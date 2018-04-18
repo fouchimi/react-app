f80f8021ce97b1e33f4eff44e3469995ea759b3a
= <ousmanefouchimi@gmail.com>
2018-04-17 20:46:52 -0600

    First commit

---
+++ book-app/src/App.js
@@ -0,0 +1,113 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { BookService } from './services/BookService';
+
+import { InputGroup, InputGroupAddon, Input, Button, Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle } from 'reactstrap';
+
+export const baseURL = 'https://www.googleapis.com/books/v1';
+export const apiKey = 'AIzaSyDRWwNOZfk4Vpmd2-sQxFs1YK269ZjAJOQ';
+export const localhostURL = 'http://localhost:3004';
+
+const SearchBar = (props) => {
+  return (
+    <div className="mainContainer">
+      <InputGroup>
+        <Input placeholder="Search book here ..." onChange={props.onChanged} />
+        <InputGroupAddon addonType="append">
+          <Button color="primary" onClick={props.onSearched}>Search</Button>
+        </InputGroupAddon>
+      </InputGroup>
+    </div>
+  );
+}
+
+const Book = props => {
+  return (
+    <div className="bookContainer">
+      <div className="book-card">
+        <Card>
+          <CardImg top width="100%" src={props.book.volumeInfo.imageLinks.thumbnail} alt="Card image cap" />
+          <CardBody>
+            <CardTitle>{props.book.volumeInfo.title}</CardTitle>
+            <CardSubtitle>{props.book.volumeInfo.subtitle}</CardSubtitle>
+            <CardText>{props.book.volumeInfo.description}</CardText>
+            <Button color="primary" onClick={props.onAddBook}>Add Book</Button>
+          </CardBody>
+        </Card>
+      </div>
+    </div>
+  );
+}
+
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      bookName: "",
+      items: []
+    }
+  }
+
+  updateInput(event){
+    this.setState({
+      bookName: event.target.value,
+    })
+  }
+
+  addBook(book) {
+      console.log(book);
+      fetch(localhostURL + '/bookshelves', {
+        method: 'POST',
+        headers: { 'content-type': 'application/json' },
+        body: book
+      }).then(response => console.log(response.json()))
+      .catch(err => console.error(err));
+  }
+
+  fetchBooks() {
+    this.state.bookName = this.state.bookName.trim().replace(/ /g, '+');
+    const url = baseURL + '/volumes' + '?q=' + this.state.bookName + '&apiKey=' + apiKey;
+
+    this._fetchBooks(url)
+      .then(response => {
+        response.items.forEach(item => {
+          item.volumeInfo.title = item.volumeInfo.title.substr(0, 30);
+          item.volumeInfo.description = item.volumeInfo.description.substr(0, 120);
+          this.setState({
+            items: response.items,
+          })
+        });
+      });
+  }
+
+  async _fetchBooks(url) {
+    try {
+      const response = await fetch(url);
+      const result = await response.json();
+      return result;
+    } catch (err) {
+      console.error(err);
+    }
+  }	
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to my bookstore</h1>
+        </header>
+        <SearchBar 
+          onChanged = {(event) => this.updateInput(event)}
+          onSearched={() => this.fetchBooks()} />
+        <div className="list">
+          {this.state.items.map((book, index) => <Book book={book} key={index} onAddBook={() => this.addBook(book) } />)}
+        </div>    
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ book-app/src/server.js
@@ -0,0 +1,47 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const path = require('path')
+const router = jsonServer.router(path.join(__dirname, 'db.json'))
+const middlewares = jsonServer.defaults();
+const fs = require('fs');
+
+server.use(middlewares);
+
+/* server.get('/echo', (req, res) => {
+    res.jsonp(req.query)
+}); */
+
+// To handle POST, PUT and PATCH you need to use a body-parser
+// You can use the one used by JSON Server
+server.use(jsonServer.bodyParser);
+
+server.post('/bookshelves', (req, res, next) => {
+    var obj = {
+        'bookshelves': []
+    };
+    //console.log(req.body);
+    obj.bookshelves.push(req.body);
+    const jsonObj = JSON.stringify(obj);
+    console.log('Updating db.json');
+    fs.writeFile('db.json', jsonObj, 'utf8', function (err) {
+        if (err) {
+            console.error(err);
+            return res.status(500).json(err);
+        }
+        console.log('Updated db.json');
+        res.send();
+    });
+    // Continue to JSON Server router
+    next()
+});
+
+server.delete('/bookshelves/:bookId', (req, res, next) => {
+    res.jsonp(req.body);
+    console.log('Book with ' + req.body + ' has been deleted successfully');
+    next();
+});
+
+server.use(router);
+server.listen(3004, () => {
+    console.log('JSON Server is running...');
+});
